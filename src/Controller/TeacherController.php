f771a782acb2987bcadd95c7b055aaa7221e725e
bacembs <bacem.bensoltana@esprit.tn>
2023-02-04 22:20:12 +0100

    atelier2

---
+++ src/Controller/TeacherController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TeacherController extends AbstractController
+{
+    #[Route('/teacher', name: 'app_teacher')]
+    public function index(): Response
+    {
+        return $this->render('teacher/index.html.twig', [
+            'controller_name' => 'TeacherController',
+        ]);
+    }
+
+    #[Route('/teachername/{name}', name: 'app_teacher_name')]
+   
+    public function showTeacher($name)
+    {
+        return $this->render('teacher/showTeacher.html.twig', [
+            'name' => $name,
+        ]);
+    }
+
+    #[Route('/teacher/index', name:'app_index')]
+  
+    public function goToIndex():Response
+    {
+        return $this->redirectToRoute('app_student');
+    }
+}
